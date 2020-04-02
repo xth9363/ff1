6fa0bf090bbaf73fc1c6d95b56e1b68593435df6
XiaTianhao <xiatianhao@guanmai.cn>
2020-04-02 15:19:31 +0800

    qp

--- hello.py
+++ hello.py
@@ -5,34 +5,37 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     print(dir(request))
     print(request.url)
     redis_conn = get_redis_conn()
-    redis_conn.set('now', str(datetime.now()))
-    return redis_conn.get('now')
+    redis_conn.set("now", str(datetime.now()))
+    return redis_conn.get("now")
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return 'login'
+    return "login"
 
 
-@app.route('/user/<username>')
+@app.route("/user/<username>")
 def profile(username):
-    return '{}\'s profile'.format(escape(username))
+    return "{}'s profile".format(escape(username))
 
 
 with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+    print(url_for("index"))
+    print(url_for("login"))
+    print(url_for("login", next="/"))
+    print(url_for("profile", username="John Doe"))
 
 
 def config_redis(conf):
     redis_conf = conf.base
     _redis_pool_db0 = redis.ConnectionPool(
-        host=redis_conf["host"], port=int(redis_conf["port"]), db=0, password=redis_conf["password"]
+        host=redis_conf["host"],
+        port=int(redis_conf["port"]),
+        db=0,
+        password=redis_conf["password"],
     )
--- t.py
+++ t.py
@@ -1,12 +1,17 @@
 from itertools import groupby
 from operator import itemgetter
 
-a = [{"n": i%2 } for i in range(10)]
-a.sort(key=itemgetter('n'))
+a = [{"n": i % 2} for i in range(10)]
+a.sort(key=itemgetter("n"))
 print(a)
 
 # for n, al in groupby(a, key=itemgetter('n')):
 #     print(len(al))
 
 
-print([{"n": len(list(al)), "n2": len(list(al))}for n, al in groupby(a, key=itemgetter('n'))])
+print(
+    [
+        {"n": len(list(al)), "n2": len(list(al))}
+        for n, al in groupby(a, key=itemgetter("n"))
+    ]
+)
